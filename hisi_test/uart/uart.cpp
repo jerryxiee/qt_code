1cd8d63666a7aabcb9df3108dbaf3227dd5cb440
abhw <liuzhenwei2013@outlook.com>
2020-10-21 16:00:43 +0800

    add uart

--- hisi_test/uart/uart.cpp
+++ hisi_test/uart/uart.cpp
@@ -10,124 +10,227 @@
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
-#define DEVICE "/dev/ttyAMA1"
+#define DEVICE "/dev/ttyAMA"
 
-#define S_TIMEOUT 1
+#define S_TIMEOUT 0
 
 static int serial_fd = 0;
 
+const char *Serial_Dev = "/dev/ttyAMA2";
+
 //打开串口并初始化设置
 
-int init_serial(void)
+int set_opt(int fd,int nSpeed,int nBits,char nEvent,int nStop)
 {
-    serial_fd = open(DEVICE, O_RDWR | O_NOCTTY | O_NDELAY);
-    if (serial_fd < 0) {
-        perror("open");
+    struct termios newtio,oldtio;
+    if(tcgetattr(fd,&oldtio)!=0)
+    {
+        perror("error:SetupSerial 3\n");
         return -1;
     }
+    bzero(&newtio,sizeof(newtio));
+    //使能串口接收
+    newtio.c_cflag |= CLOCAL | CREAD;
+    newtio.c_cflag &= ~CSIZE;
 
-    //串口主要设置结构体termios <termios.h>
-    struct termios options;
+    newtio.c_lflag &=~ICANON;//原始模式
 
-    /**1. tcgetattr函数用于获取与终端相关的参数。
-    *参数fd为终端的文件描述符，返回的结果保存在termios结构体中
-    */
-    tcgetattr(serial_fd, &options);
-    /**2. 修改所获得的参数*/
-    options.c_cflag |= (CLOCAL | CREAD);//设置控制模式状态，本地连接，接收使能
-    options.c_cflag &= ~CSIZE;//字符长度，设置数据位之前一定要屏掉这个位
-    options.c_cflag &= ~CRTSCTS;//无硬件流控
-    options.c_cflag |= CS8;//8位数据长度
-    options.c_cflag &= ~CSTOPB;//1位停止位
-    options.c_iflag |= IGNPAR;//无奇偶检验位
-    options.c_oflag = 0; //输出模式
-    options.c_lflag = 0; //不激活终端模式
-    cfsetospeed(&options, B115200);//设置波特率
+    //newtio.c_lflag |=ICANON; //标准模式
 
-    /**3. 设置新属性，TCSANOW：所有改变立即生效*/
-    tcflush(serial_fd, TCIFLUSH);//溢出数据可以接收，但不读
-    tcsetattr(serial_fd, TCSANOW, &options);
+    //设置串口数据位
+    switch(nBits)
+    {
+        case 7:
+            newtio.c_cflag |= CS7;
+            break;
+        case 8:
+            newtio.c_cflag |=CS8;
+            break;
+    }
+    //设置奇偶校验位
+    switch(nEvent)
 
+    {
+        case 'O':
+            newtio.c_cflag |= PARENB;
+            newtio.c_cflag |= PARODD;
+            newtio.c_iflag |= (INPCK | ISTRIP);
+            break;
+        case 'E':
+            newtio.c_iflag |= (INPCK | ISTRIP);
+            newtio.c_cflag |= PARENB;
+            newtio.c_cflag &= ~PARODD;
+            break;
+        case 'N':
+            newtio.c_cflag &=~PARENB;
+            break;
+    }
+    //设置串口波特率
+    switch(nSpeed)
+    {
+        case 2400:
+            cfsetispeed(&newtio,B2400);
+            cfsetospeed(&newtio,B2400);
+            break;
+        case 4800:
+            cfsetispeed(&newtio,B4800);
+            cfsetospeed(&newtio,B4800);
+            break;
+        case 9600:
+            cfsetispeed(&newtio,B9600);
+            cfsetospeed(&newtio,B9600);
+            break;
+        case 115200:
+            cfsetispeed(&newtio,B115200);
+            cfsetospeed(&newtio,B115200);
+            break;
+        case 460800:
+            cfsetispeed(&newtio,B460800);
+            cfsetospeed(&newtio,B460800);
+            break;
+        default:
+            cfsetispeed(&newtio,B9600);
+            cfsetospeed(&newtio,B9600);
+            break;
+    }
+    //设置停止位
+    if(nStop == 1)
+        newtio.c_cflag &= ~CSTOPB;
+    else if(nStop == 2)
+        newtio.c_cflag |= CSTOPB;
+    newtio.c_cc[VTIME] = 1;
+    newtio.c_cc[VMIN] = 0;
+    tcflush(fd,TCIFLUSH);
+
+    if(tcsetattr(fd,TCSANOW,&newtio)!=0)
+    {
+        perror("com set error\n");
+        return -1;
+    }
     return 0;
 }
 
-/**
-*串口发送数据
-*@fd:串口描述符
-*@data:待发送数据
-*@datalen:数据长度
-*/
-unsigned int total_send = 0 ;
-int uart_send(int fd, char *data, int datalen)
+int serial_read(int fd,unsigned char data[],int num)
 {
-    int len = 0;
-    len = write(fd, data, datalen);//实际写入的长度
-    if(len == datalen) {
-    total_send += len ;
-        printf("total_send is %d\n",total_send);
-        return len;
-    } else {
-        tcflush(fd, TCOFLUSH);//TCOFLUSH刷新写入的数据但不传送
-        return -1;
-    }
-    return 0;
+    int read_num = num;
+    int count = 3;
+    unsigned char *pdat = data;
+    int ret;
+    fd_set readfs;
+    struct timeval Timeout;
+
+    Timeout.tv_usec = 0;
+    Timeout.tv_sec  = 1;
+    FD_ZERO(&readfs);
+    FD_SET(fd, &readfs);
+
+    do
+    {
+        ret = select(fd+1, &readfs, NULL, NULL, &Timeout);
+        if(ret == 0)
+        {
+            printf("time out!\n");
+            //continue;
+            return 0;
+
+
+        }
+        else if(ret < 0)
+        {
+            printf("select error!\n");
+
+        }
+        else
+        {
+            if (FD_ISSET(fd,&readfs))
+            {
+
+                ret = read( fd, pdat, read_num);
+                read_num -= ret;
+                pdat += ret;
+                //printf("serial_read ret=%d\n",ret);
+            }
+        }
+    }while((read_num > 0)/* && (ret > 0)*/);
+    //printf("read_num=%d\n",num);
+
+    return num;
 }
 
-/**
-*串口接收数据
-*要求启动后，在pc端发送ascii文件
-*/
-unsigned int total_length = 0 ;
-int uart_recv(int fd, char *data, int datalen)
+int serial_sendx(int fd,unsigned char data[], int num)
 {
-    int len=0, ret = 0;
-    fd_set fs_read;
-    struct timeval tv_timeout;
+    int send_num = num;
+    unsigned char *pdat = data;
+    int ret;
 
-    FD_ZERO(&fs_read);
-    FD_SET(fd, &fs_read);
+    do
+    {
+        ret = write( fd,pdat, send_num);
+        send_num -= ret;
+        pdat += ret;
 
-#ifdef S_TIMEOUT
-    tv_timeout.tv_sec = (10*20/115200+2);
-    tv_timeout.tv_usec = 0;
-    ret = select(fd+1, &fs_read, NULL, NULL, NULL);
-#else
-    ret = select(fd+1, &fs_read, NULL, NULL, tv_timeout);
-#endif
+    }while(send_num > 0);
+    return 0 ;
 
-// printf("ret = %d\n", ret);
-    //如果返回0，代表在描述符状态改变前已超过timeout时间,错误返回-1
+}
 
-    if (FD_ISSET(fd, &fs_read)) {
-        len = read(fd, data, datalen);
-    total_length += len ;
-        printf("total len = %d\n", total_length);
-        return len;
-    } else {
-        perror("select");
+int uart_init(int chn,int nSpeed,int nBits,char nEvent,int nStop)
+{
+    int fd;
+    int ret;
+
+    char buff[64];
+    sprintf(buff,"%s%d",DEVICE,chn);
+
+    fd = open( buff, O_RDWR | O_NOCTTY |O_NDELAY);
+    if( fd < 0 )
+    {
+
+        printf("open uart error!\n");
         return -1;
     }
 
-    return 0;
+    ret = set_opt(fd,nSpeed,nBits,nEvent,nStop);  //设置串口
+    if(ret < 0)
+    {
+        printf("set_opt failed!\n");
+        return -1;
+    }
+    //printf("uart_init sucess\n");
+    return fd;
 }
+
 
 int uart_main(int argc, char **argv)
 {
-    init_serial();
 
-    char buf[]="hello world";
-    char buf1[11] ;
+    if ( (argc < 2))
+    {
+
+        return -1;
+    }
+
+    int chn = atoi(argv[1]);
+
+    serial_fd = uart_init(chn,9600,8,'N',1);
+
+    u_int8_t buf[]="hello world";
+    u_int8_t buf1[11] ;
     memset(buf1,0,sizeof(char)*11);
 
     while(1)
     {
-        uart_send(serial_fd, buf, 11);
-        printf("\n");
+        serial_sendx(serial_fd, buf, 2);
+//        printf("\n");
         sleep(1);
-        //if(uart_recv(serial_fd, buf1, 11) > 0)
-        uart_recv(serial_fd, buf1, 11);
-        printf("receive: %s\n", buf1);
-        memset(buf1,0,sizeof(char)*11);
+//        //if(uart_recv(serial_fd, buf1, 11) > 0)
+        int n = serial_read(serial_fd, buf1, 2);
+        if(n > 0)
+            printf("receive: %X %x\n", buf1[0],buf1[0]);
+        else {
+            printf("read null\n");
+        }
+//        memset(buf1,0,sizeof(char)*11);
     }
 
     close(serial_fd);
